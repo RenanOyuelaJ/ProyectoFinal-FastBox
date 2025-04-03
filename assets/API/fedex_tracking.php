73a21de74af17e8a8e0ef7dd8941fb83ab5f29cf
elgav04 <87767939+elgav04@users.noreply.github.com>
2025-04-03 05:53:43 +0000

    update fedex otra vez

--- assets/API/fedex_tracking.php
+++ assets/API/fedex_tracking.php
@@ -1,5 +1,5 @@
 <?php
-header("Access-Control-Allow-Origin: *");
+header("Access-Control-Allow-Origin: http://127.0.0.1:5501");
 header("Access-Control-Allow-Methods: POST, GET, OPTIONS");
 header("Access-Control-Allow-Headers: Content-Type, Authorization");
 header("Access-Control-Allow-Credentials: true");
