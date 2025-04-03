0396cfee55d87469344fe339f364e09f281b8703
elgav04 <87767939+elgav04@users.noreply.github.com>
2025-04-03 05:52:11 +0000

    update fedex tracking

--- assets/API/fedex_tracking.php
+++ assets/API/fedex_tracking.php
@@ -2,6 +2,7 @@
 header("Access-Control-Allow-Origin: *");
 header("Access-Control-Allow-Methods: POST, GET, OPTIONS");
 header("Access-Control-Allow-Headers: Content-Type, Authorization");
+header("Access-Control-Allow-Credentials: true");
 header("Content-Type: application/json");
 
 // Manejo de preflight OPTIONS request
