0c110434bc260dd733c5c5b9bc58add8484fdf47
elgav04 <87767939+elgav04@users.noreply.github.com>
2025-04-03 17:54:14 +0000

    payload prueba tarifas php

--- assets/API/fedex_tarifas.php
+++ assets/API/fedex_tarifas.php
@@ -7,10 +7,46 @@ $origenPostal = isset($_GET['origenPostal']) ? $_GET['origenPostal'] : '';
 $destinoPostal = isset($_GET['destinoPostal']) ? $_GET['destinoPostal'] : '';
 $peso = isset($_GET['peso']) ? $_GET['peso'] : '';
 
-// Devolver los datos recibidos
-echo json_encode([
-    "origenPostal" => $origenPostal,
-    "destinoPostal" => $destinoPostal,
-    "peso" => $peso
-]);
+// Crear el payload para la solicitud de tarifas
+$rate_request_data = [
+    "accountNumber" => [
+        "value" => "740561073"
+    ],
+    "rateRequestControlParameters" => [
+        "returnTransitTimes" => true
+    ],
+    "requestedShipment" => [
+        "shipper" => [
+            "address" => [
+                "postalCode" => $origenPostal,
+                "countryCode" => "US"
+            ]
+        ],
+        "recipient" => [
+            "address" => [
+                "postalCode" => $destinoPostal,
+                "countryCode" => "US"
+            ]
+        ],
+        "pickupType" => "DROPOFF_AT_FEDEX_LOCATION",
+        "shippingChargesPayment" => [
+            "paymentType" => "SENDER",
+            "payor" => [
+                "accountNumber" => "740561073", 
+                "countryCode" => "US"           
+            ]
+        ],
+        "requestedPackageLineItems" => [
+            [
+                "weight" => [
+                    "units" => "LB",
+                    "value" => $peso
+                ]
+            ]
+        ]
+    ]
+];
+
+// Mostrar el payload en formato JSON
+echo json_encode(["payload_enviado" => $rate_request_data], JSON_PRETTY_PRINT);
 ?>
