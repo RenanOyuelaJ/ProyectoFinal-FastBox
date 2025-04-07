a537e260788072ad9858fa088d23667e3225e6f4
egavarrete4 <egavarrete_0411@outlook.com>
2025-04-07 22:15:38 +0000

    reemplazo APIurl en tracking.js para despliegue

--- assets/API/tracking.js
+++ assets/API/tracking.js
@@ -1,4 +1,4 @@
-const apiUrl = "assets/API/proxy_tracking.php";
+const apiUrl = "assets/API/fedex_tracking.php";
 
 async function trackPackage() {
     const trackingNumber = document.getElementById("trackingNumber").value;
